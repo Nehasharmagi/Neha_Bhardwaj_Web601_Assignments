4a46eed7b3aa7dcddeaeb600f44b3a8d8d4e833a
Neha Sharma <NS232@myscc.ca>
2022-05-13 21:58:55 -0400

    Create AddContent

--- NSFavouriteMovies/src/app/app.component.ts
+++ NSFavouriteMovies/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Content} from "./models/content";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NSFavouriteMovies';
-}
+  MoviesContentItem: Content;
+  constructor() {
+    this.MoviesContentItem = {
+      id:1,
+      title: "Aadhaar",
+      body: "This is Drama Movie",
+      author: "Suman Ghosh",
+      type: "Drama",
+      imageUrl: "https://images.unsplash.com/photo-1618281377501-88c2328cbb9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJvYnxlbnwwfHwwfHw%3D&w=1000&q=80",
+      tags: ["Colourful","Sociable","LongLivedCreature"]
+    }
+  }
+
+  }
+
--- NSFavouriteMovies/src/app/models/content.ts
+++ NSFavouriteMovies/src/app/models/content.ts
@@ -1,2 +1,9 @@
 export interface Content {
+  readonly id: number;
+  title: string,
+  body?: string;
+  author:string;
+  type: string;
+  imageUrl?: string;
+  tags?: string[];
 }
