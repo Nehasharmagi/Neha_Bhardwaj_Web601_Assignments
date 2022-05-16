1a5ebe0b49c8149d0ae13976a1fa38eb7fbd2aee
Neha Sharma <NS232@myscc.ca>
2022-05-16 00:07:44 -0400

    Create ComponentList

--- NSFavouriteMovies/src/app/models/list.ts
+++ NSFavouriteMovies/src/app/models/list.ts
@@ -4,6 +4,7 @@ export class List {
   static contentCount = 0;
   _moives: Content[];
 
+
   constructor(item: Content) {
     this._moives = []; // initialize array
     this._moives[0] = item; // set the value of the item at the first index of the array
