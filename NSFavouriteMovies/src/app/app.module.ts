fcf0bc862564742826f6102cdae5d333e06d9e00
Neha Sharma <NS232@myscc.ca>
2022-05-16 18:39:12 -0400

    Create List With component

--- NSFavouriteMovies/src/app/app.module.ts
+++ NSFavouriteMovies/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './models/list/list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
 
   ],
   imports: [
