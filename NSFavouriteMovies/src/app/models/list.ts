ee998a2115150938a358d2cfe6c85ac45606f608
Neha Sharma <NS232@myscc.ca>
2022-05-17 21:41:53 -0400

    CommitChanges

--- NSFavouriteMovies/src/app/models/list.ts
+++ NSFavouriteMovies/src/app/models/list.ts
@@ -1,37 +1,24 @@
 import {Content} from "./content";
 
-export class List  implements Content{
-  //   A private array of type Content
-  _movies:Array<Content>;
-
-  id: number=1;
-  title: string="";
-  body?: string | undefined;
-  author: string="";
-  type: string="";
-  imageUrl?: string | undefined;
-  tags?: string[] | undefined;
-
+export class List {
+  private _movies:Content[];
   constructor() {
-    this._movies = []; // initialize array with empty string
+    this._movies=[];
+
   }
-// A getter function that returns your Content array
-  public get contentArray(){
+  get items():Content[]{
     return this._movies;
   }
 
-  // An add function that adds 1 Content item to the end of the array
-  addContent(id: number, title: string, body: string,  author: string, type:  string, imageUrl?:  string,  tags?: string[]){
-    this._movies.push({id: id, title: title, body: body,  author: author, type: type, imageUrl:  imageUrl,  tags: tags});
+  addContent(Content:any){
+    this._movies.push(Content);
+  }
+  listLength():number{
+    return this._movies.length;
+  }
+  ArrayItem(index:number){
+    return this._movies[index];
   }
 
-  // A function that returns the number of items in the array
-  public get contentArrayLength(){
-    return this.contentArray.length;
-  }
 
-  // A function that takes an input of an index of the array and returns a reader-friendly html
-  contentArrayItem(index:number){
-    return this.contentArray[index];
-  }
 }
